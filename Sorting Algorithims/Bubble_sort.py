122c6ef5e0c9e097c757342e66659c08b7509914
Sundaram Kumar Jha <93595231+sundaram2021@users.noreply.github.com>
2023-01-21 00:22:44 +0530

    Update Bubble_sort.py

--- Sorting Algorithims/Bubble_sort.py
+++ Sorting Algorithims/Bubble_sort.py
@@ -1,18 +1,21 @@
 def bubble_sort(Lists):
-    for i in range(len(Lists)):
-        for j in range(len(Lists) - 1):
-            # We check whether the adjecent number is greater or not
+    n = len(Lists)
+    swapped = False
+    for i in range(n-1):
+        for j in range(0, n-i-1):
             if Lists[j] > Lists[j + 1]:
+                swapped = True
                 Lists[j], Lists[j + 1] = Lists[j + 1], Lists[j]
+         
+        if not swapped:
+            return
 
-
-# Lets the user enter values of an array and verify by himself/herself
 array = []
 array_length = int(
-    input(print("Enter the number of elements of array or enter the length of array"))
+    input("Enter the number of elements of array or enter the length of array : ")
 )
 for i in range(array_length):
-    value = int(input(print("Enter the value in the array")))
+    value = int(input("Enter the value in the array : "))
     array.append(value)
 
 bubble_sort(array)
