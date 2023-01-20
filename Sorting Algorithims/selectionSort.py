4585044d40577a80db5fc59d62f521a593051a69
Sundaram Kumar Jha <93595231+sundaram2021@users.noreply.github.com>
2023-01-21 00:07:09 +0530

    Update selectionSort.py

--- Sorting Algorithims/selectionSort.py
+++ Sorting Algorithims/selectionSort.py
@@ -1,27 +1,20 @@
-list = []
-
-N = int(input("Enter The Size Of List"))
-
-for i in range(0, N):
-    a = int(input("Enter The number"))
-    list.append(a)
-
-
-# Let's sort list in ascending order using Selection Sort
-# Every time The Element Of List is fetched and the smallest element in remaining list is found and if it comes out
-# to be smaller than the element fetched then it is swapped with smallest number.
-
-for i in range(0, len(list) - 1):
-    smallest = list[i + 1]
-    k = 0
-    for j in range(i + 1, len(list)):
-        if list[j] <= smallest:
-            smallest = list[j]
-            k = j
-
-    if smallest < list[i]:
-        temp = list[i]
-        list[i] = list[k]
-        list[k] = temp
-
-print(list)
+# Selection sort in Python
+# time complexity O(n*n)
+#sorting by finding min_index
+def selectionSort(array, size):
+    
+    for i in range(size):
+        min_index = i
+ 
+        for j in range(i + 1, size):
+            # select the minimum element in every iteration
+            if array[j] < array[min_index]:
+                min_index = j
+         # swapping the elements to sort the array
+        (array[i], array[min_index]) = (array[min_index], array[i])
+ 
+arr = [-4, 23, 0, 10, -19,99,-12,-2,77]
+size = len(arr)
+selectionSort(arr, size)
+print('The array after selection sort is: ')
+print(arr)
